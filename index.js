b63917cd606a81f26c3ffa4c0a0a36ed53fb893a
Andrew Wilson <adwilson12901@gmail.com>
2023-10-10 20:27:30 -0400

    calculator

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+    return n - 1;
+}
+
+function makeInt(n) {
+    if (n) {
+        return parseInt(n, 10);
+    } else {
+        return NaN
+    }
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
